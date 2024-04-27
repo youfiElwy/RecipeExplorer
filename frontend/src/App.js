7a5412e59f00da4ea4a3fadf5ce245039f7eb11c
Youssef Elwy <102544122+youfiElwy@users.noreply.github.com>
2024-04-27 10:13:40 +0300

    created project (setup)

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<div>Test</div>
+			<button class="btn btn-primary">Button</button>
+		</>
+	);
+}
+
+export default App;
+
