df816d8ba3b2dde1082f2e37214e5f0d4a948626
bahylol <bahymohamed2010@gmail.com>
2024-04-29 19:24:12 +0300

    added sign in

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,6 +11,10 @@ function hashPassword(password) {
   return [salt, hash];
 }
 
+function verifyPassword(password, hash, salt) {
+  const verifyHash = crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex');
+  return verifyHash === hash;
+}
 
 const userController =
 {
@@ -20,6 +24,10 @@ const userController =
 
       if (!email) {
         return res.status(400).json({ error: 'Email is required' });
+      }
+
+      if (!userName) {
+        return res.status(400).json({ error: 'User Name is required' });
       }
 
       //check if email is already in use
@@ -49,6 +57,39 @@ const userController =
       return res.status(500).json({ error: 'Internal Server Error!' });
     }
 
+  },
+
+  signIn: async (req, res) => {
+    try {
+      const { email, password } = req.body;
+
+      if (!email) {
+        return res.status(400).json({ error: 'Email is required' });
+      }
+
+      if (!password) {
+        return res.status(400).json({ error: 'Password is required' });
+      }
+
+      //check if email exists and password matches
+      const user = await getUserByEmailDB(email).then((data) => data).catch((error) => error);
+
+      if (user.Count == 0 || !verifyPassword(password, user.Items[0].hashedPassword.S, user.Items[0].salt.S)) {
+        return res.status(400).json({ error: 'Email or password is incorrect' });
+      }
+
+      //Create jwt token and set expire date to one week
+      const today = new Date();
+      const nextWeek = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+      const token = jwt.sign({ id: user.Items[0].userID.S, expiresAt: nextWeek }, process.env.JWT, {
+        algorithm: 'HS512',
+      });
+
+      return res.cookie('jwt', token).status(200).json({ message: 'You are logged in' });
+    }
+    catch (e) {
+      return res.status(500).json({ error: 'Internal Server Error!' });
+    }
   },
 
 }
