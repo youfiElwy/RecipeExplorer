7080c2e151b917ee7412631c4b1cfd01850c1597
bahylol <bahymohamed2010@gmail.com>
2024-04-29 18:42:23 +0300

    added signup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,23 +1,55 @@
+const crypto = require('crypto');
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
-var AWS = require('aws-sdk');
-var eks = require('../eks.json');
+const { createUserDB, getUserByEmailDB } = require('../models/dynamoDB');
 
-var region = eks.AWS_REGION;
-var accessKeyId = eks.AWS_ACCESS_KEY_ID;
-var secretAccessKey = eks.AWS_SECRET_ACCESS_KEY;
-var usersTable = eks.USER_TABLE;
-var recipesTable = eks.RECIPES_TABLE;
 
-var dynamoDB = new AWS.DynamoDB({
-  region: region,
-  accessKeyId: accessKeyId,
-  secretAccessKey: secretAccessKey,
-});
+function hashPassword(password) {
+  const salt = crypto.randomBytes(16).toString('hex');
+  const hash = crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex');
+  return [salt, hash];
+}
+
 
 const userController =
 {
+  signUp: async (req, res) => {
+    try {
+      const { userName, email, password } = req.body;
+
+      if (!email) {
+        return res.status(400).json({ error: 'Email is required' });
+      }
+
+      //check if email is already in use
+      const userExists = await getUserByEmailDB(email).then((data) => data).catch((error) => error);
+
+      if (userExists.Count > 0) {
+        return res.status(400).json({ error: 'An error has occured' });
+      }
+
+
+      if (!password) {
+        return res.status(400).json({ error: 'Password is required' });
+      }
+
+      //hash and salt password
+      const hash = hashPassword(password);
+
+      const result = await createUserDB(userName, email, hash[0], hash[1]).then((data) => data).catch((error) => error);
+
+      if (!result) {
+        return res.status(500).json({ error: 'Internal Server Error!' });
+      }
+
+      return res.status(200).json({ message: 'Your account has been created please sign in!' });
+    }
+    catch (e) {
+      return res.status(500).json({ error: 'Internal Server Error!' });
+    }
+
+  },
 
 }
 
